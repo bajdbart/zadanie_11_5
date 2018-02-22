c84211e187b4e24cd57b11dd74528c1c2e334bca
Bartłomiej Bajda <allerbbb@gmail.com>
2018-02-22 03:15:14 +0000

    add corrections

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ Button.prototype = {
     var self = this;
     this.$element = $('<button>');
     this.$element.text(this.text);
-    this.$element.click(function() {
+    this.$element.on('click', function() {
 	     alert(self.text);
      });
      $('body').append(this.$element);
